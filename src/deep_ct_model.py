8030f1adeae49e7ef526b1cd0e8bed2a8f1b6bd4
Alex Lapin <arlapin@sberbank.ru>
2021-02-27 18:48:10 +0000

    Copied DeepSEA architecture

--- src/deep_ct_model.py
+++ src/deep_ct_model.py
@@ -1,1 +1,76 @@
-# TODO: Implement
+"""
+DeepCT architecture (TODO: Add our names).
+"""
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+class DeepCT(nn.Module):
+    def __init__(self, sequence_length, n_genomic_features):
+        """
+        Based on a DeepSEA architecture (see https://github.com/FunctionLab/selene/blob/0.4.8/models/deepsea.py)
+
+        Parameters
+        ----------
+        sequence_length : int
+        n_genomic_features : int
+        """
+        super(DeepCT, self).__init__()
+        conv_kernel_size = 8
+        pool_kernel_size = 4
+
+        self.conv_net = nn.Sequential(
+            nn.Conv1d(4, 320, kernel_size=conv_kernel_size),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(
+                kernel_size=pool_kernel_size, stride=pool_kernel_size),
+            nn.Dropout(p=0.2),
+
+            nn.Conv1d(320, 480, kernel_size=conv_kernel_size),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(
+                kernel_size=pool_kernel_size, stride=pool_kernel_size),
+            nn.Dropout(p=0.2),
+
+            nn.Conv1d(480, 960, kernel_size=conv_kernel_size),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5))
+
+        reduce_by = conv_kernel_size - 1
+        pool_kernel_size = float(pool_kernel_size)
+        self.n_channels = int(
+            np.floor(
+                (np.floor(
+                    (sequence_length - reduce_by) / pool_kernel_size)
+                 - reduce_by) / pool_kernel_size)
+            - reduce_by)
+        self.classifier = nn.Sequential(
+            nn.Linear(960 * self.n_channels, n_genomic_features),
+            nn.ReLU(inplace=True),
+            nn.Linear(n_genomic_features, n_genomic_features),
+            nn.Sigmoid())
+
+    def forward(self, x):
+        """Forward propagation of a batch.
+        """
+        out = self.conv_net(x)
+        reshape_out = out.view(out.size(0), 960 * self.n_channels)
+        predict = self.classifier(reshape_out)
+        return predict
+
+def criterion():
+    """
+    The criterion the model aims to minimize.
+    """
+    return nn.BCELoss()
+
+def get_optimizer(lr):
+    """
+    The optimizer and the parameters with which to initialize the optimizer.
+    At a later time, we initialize the optimizer by also passing in the model
+    parameters (`model.parameters()`). We cannot initialize the optimizer
+    until the model has been initialized.
+    """
+    return (torch.optim.SGD,
+            {"lr": lr, "weight_decay": 1e-6, "momentum": 0.9})
