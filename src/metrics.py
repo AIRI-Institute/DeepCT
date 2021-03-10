2f4230e2453480e47c693c372bc329ce1e8d47f6
Alex Lapin <arlapin@sberbank.ru>
2021-03-10 12:35:51 +0000

    Update model config

---
+++ src/metrics.py
@@ -0,0 +1,24 @@
+import numpy as np
+import sklearn.metrics as metrics
+
+### Metric helpers which convert float values to binary (0/1).
+
+def _to_binary(x: np.ndarray, threshold=0.5) -> np.ndarray:
+    return np.where(x > threshold, 1, 0).astype(int)
+
+def accuracy_score(x: np.ndarray, y: np.ndarray) -> float:
+    binary_x = _to_binary(x)
+    binary_y = _to_binary(y)
+    return metrics.accuracy_score(binary_x, binary_y)
+
+
+def precision_score(x: np.ndarray, y: np.ndarray) -> float:
+    binary_x = _to_binary(x)
+    binary_y = _to_binary(y)
+    return metrics.precision_score(binary_x, binary_y)
+
+
+def recall_score(x: np.ndarray, y: np.ndarray) -> float:
+    binary_x = _to_binary(x)
+    binary_y = _to_binary(y)
+    return metrics.recall_score(binary_x, binary_y)
