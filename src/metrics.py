25fdfdfe1326f255c61ec00921bcee3b1aa52c90
Alex Lapin <arlapin@sberbank.ru>
2021-03-11 14:02:58 +0000

    Lint

--- src/metrics.py
+++ src/metrics.py
@@ -3,8 +3,10 @@ import sklearn.metrics as metrics
 
 ### Metric helpers which convert float values to binary (0/1).
 
+
 def _to_binary(x: np.ndarray, threshold=0.5) -> np.ndarray:
     return np.where(x > threshold, 1, 0).astype(int)
+
 
 def accuracy_score(x: np.ndarray, y: np.ndarray) -> float:
     binary_x = _to_binary(x)
