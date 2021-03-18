c9a2ff7458276c508b872339cc47fa333821f477
Alex Lapin <arlapin@sberbank.ru>
2021-03-18 12:27:27 +0000

    Make embedding lengths configurable

--- src/tests/deep_ct_model_test.py
+++ src/tests/deep_ct_model_test.py
@@ -6,6 +6,8 @@ class TestDeepCT:
         cmd = DeepCT(
             sequence_length=1000,
             n_cell_types=500,
+            sequence_embedding_length=128,
             cell_type_embedding_length=20,
+            final_embedding_length=128,
             n_genomic_features=1,
         )
