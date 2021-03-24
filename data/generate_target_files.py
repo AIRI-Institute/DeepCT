3c7bfa86b4d01cf6a23b696193664c804993af84
Maria Sindeeva <MASindeeva@sberbank.ru>
2021-03-24 13:39:59 +0000

    add chromosome ends to blacklist & update test data

--- data/generate_target_files.py
+++ data/generate_target_files.py
@@ -1,7 +1,7 @@
 from utils import full_target_file_pipeline
 
 fasta_path = "/mnt/datasets/DeepCT/male.hg19.fasta"
-encode_blacklist_path = "hg19_blacklist_ENCFF001TDO.bed"
+encode_blacklist_path = "long_hg19_blacklist_ENCFF001TDO.bed"
 all_targets_path = "/mnt/datasets/DeepCT/all_features/sorted_deepsea_data.bed"
 
 full_target_file_pipeline(
@@ -12,4 +12,6 @@ full_target_file_pipeline(
     target_path="sorted_data.bed",
     target_sampling_intervals_path="target_intervals.bed",
     distinct_features_path="distinct_features.txt",
+    elongate_encode_blacklist=False,
+    interval_padding=500,
 )
