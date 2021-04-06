bd85812b6e781ce67a4c110e8a2bf3c12db5a47b
Maria Sindeeva <MASindeeva@sberbank.ru>
2021-04-06 12:39:05 +0000

    update tests

--- src/tests/dataset_test.py
+++ src/tests/dataset_test.py
@@ -2,35 +2,38 @@ import os
 
 from src.dataset import EncodeDataset
 
+TEST_DATA_PATH = "data/test_data/"
+
 
 class TestEncodeDataset:
-    test_data_path = "data/test_data/"
-
-    def test_initilization(self):
+    def setup_method(self, test_method):
         # read test intervals
-        sampling_intervals_path = os.path.join(
-            self.test_data_path, "target_intervals.bed"
-        )
+        sampling_intervals_path = os.path.join(TEST_DATA_PATH, "target_intervals.bed")
         intervals = []
         with open(sampling_intervals_path) as f:
             for line in f:
                 chrom, start, end = line.split()
                 intervals.append((chrom, int(start), int(end)))
+
         # read distinct features
-        distinct_features_path = os.path.join(
-            self.test_data_path, "distinct_features.txt"
-        )
+        distinct_features_path = os.path.join(TEST_DATA_PATH, "distinct_features.txt")
         with open(distinct_features_path) as f:
             distinct_features = list(map(lambda x: x.rstrip(), f.readlines()))
 
+        # read target features
+        target_features_path = os.path.join(TEST_DATA_PATH, "target_features.txt")
+        with open(target_features_path) as f:
+            target_features = list(map(lambda x: x.rstrip(), f.readlines()))
+
+        reference_sequence_path = os.path.join(TEST_DATA_PATH, "mini_male.hg19.fasta")
+        target_path = os.path.join(TEST_DATA_PATH, "mini_sorted_data.bed.gz")
+
         # initialize the cell-wise dataset
-        dataset = EncodeDataset(
-            reference_sequence_path=os.path.join(
-                self.test_data_path, "mini_male.hg19.fasta"
-            ),
-            target_path=os.path.join(self.test_data_path, "mini_sorted_data.bed.gz"),
+        self.cellwise_dataset = EncodeDataset(
+            reference_sequence_path=reference_sequence_path,
+            target_path=target_path,
             distinct_features=distinct_features,
-            target_features=["DNase"],
+            target_features=target_features,
             intervals=intervals,
             cell_wise=True,
             transform=None,
@@ -41,13 +44,11 @@ class TestEncodeDataset:
         )
 
         # initialize the dataset without cell types
-        dataset = EncodeDataset(
-            reference_sequence_path=os.path.join(
-                self.test_data_path, "mini_male.hg19.fasta"
-            ),
-            target_path=os.path.join(self.test_data_path, "mini_sorted_data.bed.gz"),
+        self.cellfree_dataset = EncodeDataset(
+            reference_sequence_path=reference_sequence_path,
+            target_path=target_path,
             distinct_features=distinct_features,
-            target_features=["DNase"],
+            target_features=target_features,
             intervals=intervals,
             cell_wise=False,
             transform=None,
@@ -56,3 +57,19 @@ class TestEncodeDataset:
             feature_thresholds=0.5,
             strand="+",
         )
+
+    def test_sample__cellfree_shape(self):
+        sample = self.cellfree_dataset[100]
+        assert len(sample[1]) == len(self.cellfree_dataset.distinct_features)
+
+    def test_sample__cellwise_positive(self):
+        sample = self.cellwise_dataset[
+            100 * len(self.cellfree_dataset.distinct_features) + 1
+        ]
+        assert sample[2] == 1
+
+    def test_sample__cellwise_negative(self):
+        sample = self.cellwise_dataset[
+            100 * len(self.cellfree_dataset.distinct_features)
+        ]
+        assert sample[2] == 0
